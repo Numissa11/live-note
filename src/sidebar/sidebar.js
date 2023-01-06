86d9ee6c0181f4014ac907f626275219a4fa57eb
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2023-01-06 01:42:35 +0100

    add title

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -17,15 +17,31 @@ export class Sidebar extends React.Component {
             <div className='sidebarContainer'>
                 <button
                     onClick={this.newNoteBtnClick}
-                    className='newNotBtn'
-                >  New Note
+                    className='newNotBtn' > New Note
                 </button>
+                {
+                    this.state.addingNote ?
+                        <div>
+                            <input type='text'
+                                className='newNoteInput'
+                                placeholder='Enter note title'
+                                onKeyUp={(e) => this.updateTitle(e.target.value)}>
+                            </input>
+                        </div> :
+                        null
+                }
             </div>
         )
     }
 
     newNoteBtnClick = () => {
+        console.log('new note')
         this.setState({ title: null, addingNote: !this.state.addingNote });
+    }
+
+    updateTitle = (txt) => {
+        // this.setState({ title: txt });
+        console.log('here is my txt:', txt)
       }
 }
 
